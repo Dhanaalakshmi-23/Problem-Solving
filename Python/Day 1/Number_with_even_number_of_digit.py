8ccc0d6d746a9eb9e31773bd96049c1b1083667a
Dhanaa Lakshmi <dhanaalakshminarayanan@gmail.com>
2026-04-24 20:05:20 +0530

    Numbers with even number of digits Solved

---
+++ Python/Day 1/Number_with_even_number_of_digit.py
@@ -0,0 +1,22 @@
+def findNumbers(nums):
+    count = 0
+
+    for i in range(len(nums)):
+        digit = 0
+        n = nums[i]
+
+        while n != 0:
+            n = n // 10
+            digit += 1
+
+        if digit % 2 == 0:
+            count += 1
+
+    return count
+
+
+nums_input = [1, 22, 3456, 666]
+
+result = findNumbers(nums_input)
+
+print(result)
